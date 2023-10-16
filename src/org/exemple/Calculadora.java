470e907f2ebf64b4a183fb1cf730dd0c2bd8739c
João Baptista Fernando de Bragança TeixeIra <joao.bbraganca@al.infnet.edu.br>
2023-10-16 11:39:17 -0300

    trabalho em sala de aula

---
+++ src/org/exemple/Calculadora.java
@@ -0,0 +1,25 @@
+package org.exemple;
+
+public class Calculadora {
+
+
+    public static double Somar(double n1 , double n2){
+        return n1 + n2;
+    }
+
+    public static double Subtrair(double n1 , double n2){
+        return n1 - n2;
+    }
+
+    public static double Multiplicar(double n1 , double n2){
+        return n1 * n2;
+    }
+
+    public static double Dividir(double n1 , double n2){
+        return n1 / n2;
+    }
+
+
+
+}
+
